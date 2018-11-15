05e4024e8a3491d52b98b65ad49a61e66036172c
shohruh_abduakhatov <shokhrukh.abduahadov@gmail.com>
2018-11-15 09:08:55 +0500

    Firebase added

---
+++ app/src/main/java/uz/radar/wiut/radar/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package uz.radar.wiut.radar.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uz.radar.wiut.radar.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
