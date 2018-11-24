fd1940080bbd09fa645efb84415e1b9614bdf9d2
shohruh_abduakhatov <shokhrukh.abduahadov@gmail.com>
2018-11-24 18:27:48 +0500

    Splash Screen needs to be finished

--- app/src/main/java/uz/radar/wiut/radar/activity/SplashActivity.java
+++ app/src/main/java/uz/radar/wiut/radar/activity/SplashActivity.java
@@ -3,13 +3,60 @@ package uz.radar.wiut.radar.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import uz.radar.wiut.radar.R;
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
 
-public class SplashActivity extends AppCompatActivity {
+import uz.radar.wiut.radar.R;
+import uz.radar.wiut.radar.utils.Const;
+
+public class SplashActivity extends AwesomeSplash {
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+
+        /* you don't have to override every property */
+
+        //Customize Circular Reveal
+        configSplash.setBackgroundColor(R.color.primary); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(2000); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
+
+        //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
+
+        //Customize Logo
+        configSplash.setLogoSplash(R.mipmap.ic_launcher); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(2000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.Bounce); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+
+
+        //Customize Path
+        configSplash.setPathSplash(Const.DROID_LOGO); //set path String
+        configSplash.setOriginalHeight(400); //in relation to your svg (path) resource
+        configSplash.setOriginalWidth(400); //in relation to your svg (path) resource
+        configSplash.setAnimPathStrokeDrawingDuration(3000);
+        configSplash.setPathSplashStrokeSize(3); //I advise value be <5
+        configSplash.setPathSplashStrokeColor(R.color.accent); //any color you want form colors.xml
+        configSplash.setAnimPathFillingDuration(3000);
+        configSplash.setPathSplashFillColor(R.color.Wheat); //path object filling color
+
+
+        //Customize Title
+        configSplash.setTitleSplash("My Awesome App");
+        configSplash.setTitleTextColor(R.color.Wheat);
+        configSplash.setTitleTextSize(30f); //float value
+        configSplash.setAnimTitleDuration(3000);
+        configSplash.setAnimTitleTechnique(Techniques.FlipInX);
+        configSplash.setTitleFont("fonts/myfont.ttf"); //provide string to your font located in assets/fonts/
+
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+    public void animationsFinished() {
+
+        //transit to another activity here
+        //or do whatever you want
     }
 }
+
